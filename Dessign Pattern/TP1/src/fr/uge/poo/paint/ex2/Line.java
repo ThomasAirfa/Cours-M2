21fd7308dc753350327b122d0ff7b581d9887219
Thomas <thomasguillotin@free.fr>
2026-09-25 16:12:43 +0200

    Design pattern TP1 ex 2

---
+++ Dessign Pattern/TP1/src/fr/uge/poo/paint/ex2/Line.java
@@ -0,0 +1,21 @@
+package fr.uge.poo.paint.ex2;
+
+import java.awt.Graphics2D;
+import java.util.Objects;
+
+public record Line(int x1, int y1, int x2, int y2) {
+
+  public static Line parse(String text) {
+	Objects.requireNonNull(text);
+	var tokens = text.split(" ");
+	int x1 = Integer.parseInt(tokens[1]);
+	int y1 = Integer.parseInt(tokens[2]);
+	int x2 = Integer.parseInt(tokens[3]);
+	int y2 = Integer.parseInt(tokens[4]);
+	return new Line(x1, y1, x2, y2);
+  }
+
+  public void draw(Graphics2D graphics) {
+	graphics.drawLine(x1, y1, x2, y2);
+  }
+}
---
+++ Dessign Pattern/TP1/src/fr/uge/poo/paint/ex2/Paint.java
@@ -0,0 +1,32 @@
+package fr.uge.poo.paint.ex2;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import fr.uge.poo.simplegraphics.SimpleGraphics;
+
+public class Paint {
+
+  public static void main(String[] args) throws IOException {
+	if (args.length != 1) {
+	  System.err.println("Missing file. Usage : java -cp bin fr.uge.poo.paint.ex2.Paint <file>");
+	  return;
+	}
+
+	var path = Path.of(args[0]);
+	var linesArray = new ArrayList<Line>();
+	try (var lines = Files.lines(path)) {
+	  lines.forEach(line -> linesArray.add(Line.parse(line)));
+	}
+
+	SimpleGraphics area = new SimpleGraphics("area", 800, 600);
+	area.clear(Color.WHITE);
+	area.render(graphics -> {
+	  graphics.setColor(Color.black);
+	  linesArray.forEach(line -> line.draw(graphics));
+	});
+  }
+}
